869bcb1a8e4c71a878f9431d39e19e07a28d879b
r3dby73 <bunnypart@protonmail.com>
2022-09-27 21:33:11 +0300

    Show database of users to main page

--- js/users.js
+++ js/users.js
@@ -6,7 +6,28 @@ window.onload = function(event) {
         method: 'GET',
         dataType: 'json',
         success: function(data) {
-
+            data.users.forEach(function(user) {
+                if(data.login == user['login'])
+                {
+                    $('table').append(
+                        '<tr class="auth_user">' +
+                            '<td>' + user['login'] + '</td>' +
+                            '<td>' + user['password'] + '</td>' +
+                            '<td>' + user['email'] + '</td>'
+                        + '</tr>'
+                    );
+                }
+                else
+                {
+                    $('table').append(
+                        '<tr>' +
+                            '<td>' + user['login'] + '</td>' +
+                            '<td>' + user['password'] + '</td>' +
+                            '<td>' + user['email'] + '</td>'
+                        + '</tr>'
+                    );
+                }
+            });
         }
     });
 };
