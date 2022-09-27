9e66254137a1e236eec4690c9e085ece82c3b5b7
r3dby73 <bunnypart@protonmail.com>
2022-09-27 14:41:02 +0300

    Add function to check error in array of errors in register response

--- js/register.js
+++ js/register.js
@@ -1,11 +1,19 @@
 document.forms.register_form.onsubmit = function(event) {
     event.preventDefault();
-    
+
     let login = document.forms.register_form.login.value;
         password = document.forms.register_form.password.value,
         confirm_password = document.forms.register_form.confirm_password.value,
         email = document.forms.register_form.email.value;
-    
+
+    function inArray(arr)
+    {
+        for(let i = 1; i < arguments.length; i++)
+            if($.inArray(arguments[i], arr) != -1)
+                return true;
+        return false;
+    }
+
     $.ajax({
         url: '../php/register.php',
         method: 'POST',
