eff60f75ba6bb5aba91b6cf63fede9830340c9d9
r3dby73 <bunnypart@protonmail.com>
2022-09-27 13:46:58 +0300

    Check user input from register page

--- php/register.php
+++ php/register.php
@@ -1,8 +1,21 @@
 <?php
 
+require_once '../classes/RegisterUser.php';
+
 $login = trim(htmlspecialchars($_POST['login']));
 $password = trim(htmlspecialchars($_POST['password']));
 $confirm_password = trim(htmlspecialchars($_POST['confirm_password']));
 $email = trim(htmlspecialchars($_POST['email']));
+
+$user = new RegisterUser($login, $password, $confirm_password, $email);
+
+$user->checkEmptyInputs();
+$user->checkLoginLen();
+$user->checkLogin();
+$user->checkPasswordLen();
+$user->checkStrongPassword();
+$user->comparePasswords();
+$user->checkEmail();
+$user->checkUniq();
 
 ?>
