0f771b8324ecc3d07bb04c3ca4f64332a17f5937
r3dby73 <bunnypart@protonmail.com>
2022-09-27 19:08:45 +0300

    Check user input from login page

--- php/login.php
+++ php/login.php
@@ -1,6 +1,15 @@
 <?php
 
+require_once '../classes/LoginUser.php';
+
 $login = trim(htmlspecialchars($_POST['login']));
 $password = trim(htmlspecialchars($_POST['password']));
+
+$user = new LoginUser($login, $password);
+
+$user->checkEmptyInputs();
+$user->isExists();
+
+echo json_encode(['errors' => $user->getErrors()]);
 
 ?>
