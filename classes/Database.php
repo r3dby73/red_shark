e678cd135c396278d6e0e3c843563e48cfa2ee52
r3dby73 <bunnypart@protonmail.com>
2022-09-27 13:48:49 +0300

    Add function to add new user in Database class

--- classes/Database.php
+++ classes/Database.php
@@ -4,6 +4,30 @@ class Database
 {
     private string $db_path = '../db/users.json';
     private string $salt = 'Red_Shark';
+
+    public function addNewUser(string $login, string $password, string $email)
+    {
+        if(!file_exists($this->db_path))
+        {
+            $db['redShark']['users'] = [[
+                'login' => $login,
+                'password' => md5($this->salt.$password),
+                'email' => $email
+            ]];
+            file_put_contents($this->db_path, json_encode($db));
+        }
+
+        else
+        {
+            $db = json_decode(file_get_contents($this->db_path), true);
+            $db['redShark']['users'][] = [
+                'login' => $login,
+                'password' => md5($this->salt.$password),
+                'email' => $email
+            ];
+            file_put_contents($this->db_path, json_encode($db));
+        }
+    }
 }
 
 ?>
