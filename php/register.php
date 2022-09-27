c88da1f9b157e1b2647a7811d35b47265b2faf41
r3dby73 <bunnypart@protonmail.com>
2022-09-27 13:46:31 +0300

    Get user input from register page

---
+++ php/register.php
@@ -0,0 +1,8 @@
+<?php
+
+$login = trim(htmlspecialchars($_POST['login']));
+$password = trim(htmlspecialchars($_POST['password']));
+$confirm_password = trim(htmlspecialchars($_POST['confirm_password']));
+$email = trim(htmlspecialchars($_POST['email']));
+
+?>
